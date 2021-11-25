460c31fd3450486072349218e9f7918fa04f2580
mariklim <maria.klimenko889@gmail.com>
2021-11-25 17:03:41 +0100

    add const useStyles

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,7 +1,22 @@
 import JobList from '../data/JobList.json';
 import JobIcon from '../img/job_icon_1.png';
+import {makeStyles, useTheme} from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  
+    coloredLink: {
+        background: `-webkit-linear-gradient(left, #40C5E4 15.51%, #DE8E8E 77.84%)`,
+        "-webkit-background-clip": "text",
+        "-webkit-text-fill-color": "transparent",
+        display: "inline-block",
+        '&:hover': {
+            cursor: "pointer",
+        }
+    },
+}))
 
 function Job() {
+    const classes = useStyles()
     return (
         <div>
             {
@@ -17,7 +32,7 @@ function Job() {
                                 <span>{job.tags}</span>
                             </div>
                         </div>
-                        <a href={job.link} target="_blank">Apply now</a>
+                        <a  className={classes.coloredLink} href={job.link} target="_blank">Apply now</a>
 
                     </div>
                 })
