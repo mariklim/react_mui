27a7dbdddee67998d4f34c2235db2dd17a14d917
mariklim <maria.klimenko889@gmail.com>
2021-11-25 16:25:51 +0100

    add cards

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,11 +1,25 @@
 import JobList from '../data/JobList.json';
+import JobIcon from '../img/job_icon_1.png';
 
 function Job() {
     return (
         <div>
             {
                 JobList.map((job, index) => {
-                    return <p key={index}>{job.name}</p>
+                    return <div key={index}
+                        style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}
+                    >
+
+                        <img src={JobIcon} alt="job icon" />
+                        <div className="job_description">
+                            <h2>{job.name}</h2>
+                            <div className="tags">
+                                <span>{job.tags}</span>
+                            </div>
+                        </div>
+                        <a href={job.link} target="_blank">Applay now</a>
+
+                    </div>
                 })
             }
 
