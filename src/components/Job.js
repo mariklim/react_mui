d7055584f34f0bded4c0413d98ebcb3651b45602
mariklim <maria.klimenko889@gmail.com>
2021-11-26 12:24:48 +0100

    add .map for tags

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,13 +1,13 @@
 import JobList from '../data/JobList.json';
 import JobIcon from '../img/job_icon_1.png';
 import BgHover from '../img/bg_hover.png';
-import {makeStyles} from "@mui/styles";
+import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
-  
+
     coloredLink: {
-        
-        fontSize: '20px',    
+
+        fontSize: '20px',
         background: `-webkit-linear-gradient(left, #40C5E4 15.51%, #DE8E8E 77.84%)`,
         "-webkit-background-clip": "text",
         "-webkit-text-fill-color": "transparent",
@@ -16,59 +16,66 @@ const useStyles = makeStyles((theme) => ({
             cursor: "pointer",
         }
     },
-    jobRow:{
-        height:'88px',
-        paddingRight:'40px',
-        paddingLeft:'15px',
-        display: 'flex', 
-        alignItems: 'center', 
+    jobRow: {
+        height: '88px',
+        paddingRight: '40px',
+        paddingLeft: '15px',
+        display: 'flex',
+        alignItems: 'center',
         justifyContent: 'center',
-        margin:'20px',
-        fontFamily:'Jost',
-        color:'#001921',
-        transition: 'transform .2s',
+        margin: '20px',
+        fontFamily: 'Jost',
+        color: '#001921',
         fontSize: '16px',
         '&:hover': {
             cursor: "pointer",
-            backgroundImage:`url(${BgHover})`, 
-            backgroundRepeat  : 'no-repeat',
+            backgroundImage: `url(${BgHover})`,
+            backgroundRepeat: 'no-repeat',
             backgroundSize: 'contain',
             transform: 'scale(1.02)'
         }
     },
-    tags:{
+    tags: {
+        display: 'inline-block',
         fontSize: '14px',
-        fontFamily:'Exo2',
-        color:'#636465',
-        fontWeight: 'Extra-light'
+        fontFamily: 'Exo2',
+        color: '#636465',
+        fontWeight: 'Extra-light',
+        padding: '0px 4px',
+        lineHeight: '1px'
     }
 }))
-
 function Job() {
     const classes = useStyles()
     return (
         <div>
             {
                 JobList.map((job, index) => {
-                    return <div key={index}
-                    className={classes.jobRow}
-                    >
 
+                    return <div key={index} className={classes.jobRow}>
                         <img src={JobIcon} alt="job icon" />
-                        <div style={{marginRight:'auto', marginLeft:'30px', lineHeight:'10px'}}>
+                        <div style={{ marginRight: 'auto', marginLeft: '30px', lineHeight: '1px' }}>
                             <h2>{job.name}</h2>
-                         
-                                <p className={classes.tags}>{job.tags}</p>
-                           
-                        </div>
-                        <a  className={classes.coloredLink} href={job.link} target="_blank">Apply now</a>
 
+                            {/* ciclo per tags */}
+                            {
+                                job.tags.map((tag) => {
+                                    return <p className={classes.tags} >{tag}</p>
+                                })
+                            }
+                        </div>
+                        <a className={classes.coloredLink} href={job.link} target="_blank">Apply now</a>
                     </div>
                 })
             }
+
 
         </div>
     );
 }
 
 export default Job;
+{/* 
+                            <span  className={classes.tags}>{job.tags[0]}</span>
+                            <span  className={classes.tags}>{job.tags[1]}</span>
+                            <span  className={classes.tags}>{job.tags[2]}</span> */}
