89a3ae2b8fd4af4e49c56a2f3dce089e1467a267
mariklim <maria.klimenko889@gmail.com>
2021-11-26 10:12:10 +0100

    add container for Job.js

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,29 +1,55 @@
 import Grid from '@mui/material/Grid';
 import jobHero from '../img/job_hero.png';
 import Job from '../components/Job';
+import { Box, Container, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
+const useStyles = makeStyles((theme) => (
+    {
+        container: {
 
+            display: "flex",
+       
+
+        },
+       
+    }
+))
 
 function Jobs() {
+    const classes = useStyles()
     return (
-        <div>
-            <Grid container spacing={0} style={{padding:'30px'}}>
+        <Container maxWidth={"xl"}>
+            <Grid container spacing={0} style={{ padding: '30px', display: "flex", justifyContent: 'center' }}>
                 <Grid item xs={6} style={{
-                    textAlign:'center'
+
                 }}>
-                    <img src={jobHero} alt="join us" />
+                    <Typography paddingLeft="36px" paddingTop="24px" variant={"h2"}>
+                        /Work with us
+                    </Typography>
                 </Grid>
-                <Grid item xs={6}  style={{
-                    textAlign:'center'
-                }}>
-                     <Job/>
+                <Grid item xs={6}>
+                    <Box component="div" sx={{width:'50%' }}>
+                    <Typography paddingLeft="36px" paddingTop="24px" variant={"body1"}>
+                        Hey! We’re hiring!
+                        The following positions are currently available.
+                        Click on the link to see the job offer.
+                    </Typography>
+                    </Box>
                 </Grid>
             </Grid>
 
+            <Grid container spacing={0} style={{ padding: '30px', display: "flex" }}>
+                <Grid item xs={6} style={{
 
-
-
-        </div>
+                }}>
+                    <img src={jobHero} alt="join us" />
+                </Grid>
+                <Grid item xs={6}>
+                    <Job />
+                </Grid>
+            </Grid>
+        </Container>
     )
 };
 
